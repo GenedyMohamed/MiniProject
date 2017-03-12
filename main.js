e3e676ac1e4eab155fc5495a308305b7a2546614
Genedy <genedymohamed96@gmail.com>
2017-03-12 16:34:54 +0200

    commit

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 var express = require('express');
 var router = require('./app/routes');
 // var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
-// var DB_URI = "mongodb://localhost:27017/portfolio";
+ var mongoose = require('mongoose');
+ var DB_URI = "mongodb://localhost:27017/portfolio";
 // var cookieParser = require('cookie-parser');
 // var sessions = require('express-session');
 // var session = require('client-sessions');
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 // app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname+ '/public'));
 
-// mongoose.connect(DB_URI);
+ mongoose.connect(DB_URI);
  app.use(router);
 // app.use(cookieParser());
 // app.use(session({secret: "Shh, its a secret!"}));
